062cb65cce59c447b1cafce2847ac8f027e7b20b
real-rpj11 <precy-jane.roxas@student.passerellesnumeriques.org>
2021-01-19 18:00:55 +0800

    nuke and start again

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -6,13 +6,15 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="" method="post">
+    <form action="{{url('bus}}" method="post">
     <label for="busname">Bus Name:</label>
-    <input type="text" name="bus_name" palceholder="Bus Name"><br>
+    <input type="text" name="bus_name" placeholder="Bus Name"><br>
+    <br>
     <label for="passenger">No. of Passengers:</label>
     <input type="number" placeholder="No. of Passengers" name="number_of_passenger"><br>
+    <br>
     <label for="price">Price/Day</label>
-    <input type="number" name="price/day" placeholder="Price per Day">
+    <input type="number" name="price/day" placeholder="Price per Day"><br>
     <br>
     <input type="submit" value="Submit" name="submit_btn">
     </form>

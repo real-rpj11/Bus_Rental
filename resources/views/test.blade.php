65ff969017391274535f188059e7860fec09a889
real-rpj11 <precy-jane.roxas@student.passerellesnumeriques.org>
2021-01-19 16:33:44 +0800

    test

---
+++ resources/views/test.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+    <label for="busname">Bus Name:</label>
+    <input type="text" name="bus_name" palceholder="Bus Name"><br>
+    <label for="passenger">No. of Passengers:</label>
+    <input type="number" placeholder="No. of Passengers" name="number_of_passenger"><br>
+    <label for="price">Price/Day</label>
+    <input type="number" name="price/day" placeholder="Price per Day">
+    <br>
+    <input type="submit" value="Submit" name="submit_btn">
+    </form>
+</body>
+</html>
